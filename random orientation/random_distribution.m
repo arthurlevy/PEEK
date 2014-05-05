1f450a378d6f367a80d3c108566c2556bcd37efd
Arthur Levy <arthur.levy@mcgill.ca>
2014-05-05 16:24:59 -0400

    changed distribution alogithm.
    Now when a strand is mispositionned (across wall), it position is perturbed untill it fits.

--- random orientation/random_distribution.m
+++ random orientation/random_distribution.m
@@ -10,7 +10,7 @@ Dx = 4*25e-3;%m
 Dy = 4*25e-3;%m
 
 %% number of strands to distribute
-Ns = 5000;
+Ns = 100000;
 
 %% strands dimensions
 L = 0.5*25.4e-3;%m length
@@ -24,11 +24,13 @@ configuration = zeros(Ns,3);
 
 %% loop for each strand
 for i = 1:Ns
-	positionned = false; %we did not manage yet to position it
 	
+	%suggest initial position
+	[x, y,theta] = through_dice(Dx,Dy);
+	positionned = is_it_possible(x,y,theta,Dx,Dy, L,w);
 	while (~positionned) % try to position strand i	
 		%suggest a position
-		[x, y, theta] = through_dice(Dx,Dy);
+		[x, y,theta] = modify_position(x, y,theta, Dx,Dy);
 		%test if it is not braking constrains
 		positionned = is_it_possible(x,y,theta,Dx,Dy, L,w);
 		
@@ -54,8 +56,25 @@ end
 function [x, y, theta] = through_dice(Dx,Dy)
 x = rand * Dx;
 y = rand * Dy;
-theta = rand*pi; %theta should be between 0 and 180 deg
+theta = rand*2*pi; %theta should be between 0 and 360 deg
 end
+
+
+function [x, y,theta] = modify_position(x, y,theta, Dx,Dy)
+
+dx = rand*Dx/30 - Dx/60;
+dy = rand*Dy/30 - Dy/60;
+dtheta = rand*2*pi/30;
+
+x = x+dx;
+y = y+dy;
+if (x<0 || x>Dx); x=x-2*dx; end
+if (y<0 || y>Dy); y=y-2*dy; end
+theta = theta + dtheta;
+
+end
+
+
 
 
 %check if a configuration given by x, y and theta is possible (not
@@ -64,10 +83,10 @@ end
 function positionned = is_it_possible(x,y,theta,Dx,Dy,L,w)
 
 positionned = ...
-	(   (x - abs(cos(theta)*L))    >  0 ) * ... not crossing left wall
-	(   (x + abs(cos(theta)*L))    <  Dx) * ... not crossing right wall
-	(   (y - sin(theta)*L)    >  0 ) * ... not crossing lower wall
-	(   (y + sin(theta)*L)    <  Dy) ;%    not crossing upper wall
+	(   (x - abs(cos(theta)*L/2))    >  0 ) * ... not crossing left wall
+	(   (x + abs(cos(theta)*L/2))    <  Dx) * ... not crossing right wall
+	(   (y - abs(sin(theta)*L/2))    >  0 ) * ... not crossing lower wall
+	(   (y + abs(sin(theta)*L/2))    <  Dy) ;%    not crossing upper wall
 
 %note that currently, we consider "slender" strands (ie w = 0);
 end
